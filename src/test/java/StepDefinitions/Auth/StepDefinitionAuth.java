31752a4209b59a200153e4b04bcfd27afb09c41c
subha sree.N <subhasreen341@gmail.com>
2026-04-22 09:04:10 +0530

    AUTH module completed

--- src/test/java/StepDefinitions/Auth/StepDefinitionAuth.java
+++ src/test/java/StepDefinitions/Auth/StepDefinitionAuth.java
@@ -1,80 +1,120 @@
 package StepDefinitions.Auth;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.testng.Assert;
 
 import io.cucumber.java.en.*;
+import io.cucumber.datatable.DataTable;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+
+import Constants.Endpoints;
+import Utility.ExcelUtil;
+
 public class StepDefinitionAuth {
-	
-	Response response;
-    PojoClasses.Auth authObj;
 
-    
-    @Given("BaseURL is set for login")
-    public void setBaseURI() {
-        RestAssured.baseURI = "https://fakestoreapi.com";
-    }
+    Response response;
+    Map<String, Object> requestMap;   
 
    
-    @Given("I have valid login credentials")
-    public void setLoginPayload() {
 
-        authObj = new PojoClasses.Auth("mor_2314", "83r5^_");
-    }
+    @Given("I read login data from excel row {string}")
+    public void readLoginData(String rowNumber) throws IOException {
 
-  
-    @When("I send a POST request to {string} for login")
-    public void sendLoginRequest(String endpoint) {
+        int rowNum = Integer.parseInt(rowNumber);
 
-        response = RestAssured.given()
-                .header("Content-Type", "application/json")
-                .body(authObj)   
-                .post(endpoint);
-    }
-    
-    @Given("I have login credentials with missing username")
-    public void setInvalidLoginPayload() {
+        String path = System.getProperty("user.dir") + "/src/test/resources/Data/AUTH_TestCases.xlsx";
+        String sheet = "Sheet1";  
+        String username = ExcelUtil.getCellData(path, sheet, rowNum, 0);
+        String password = ExcelUtil.getCellData(path, sheet, rowNum, 1);
 
-        
-        authObj = new PojoClasses.Auth(null, "83r5^_");
-    }
-   
-    @Given("I have login credentials with missing password")
-    public void setLoginPayloadMissingPassword() {
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
 
-        authObj = new PojoClasses.Auth("mor_2314", null);
-    }
-    
- 
-    @Given("I have empty login request body")
-    public void setEmptyLoginPayload() {
-        authObj = null;   
-    }
+        requestMap = new HashMap<>();
 
-    
-    @When("I send a POST request to {string} for login with empty body")
-    public void sendLoginRequest1(String endpoint) {
+        if (username != null && !username.isEmpty()) {
+            requestMap.put("username", username);
+        }
 
-        if (authObj == null) {
-            
-            response = RestAssured.given()
-                    .header("Content-Type", "application/json")
-                    .body("{}")
-                    .post(endpoint);
-        } else {
-            response = RestAssured.given()
-                    .header("Content-Type", "application/json")
-                    .body(authObj)
-                    .post(endpoint);
+        if (password != null && !password.isEmpty()) {
+            requestMap.put("password", password);
         }
     }
-    
+
+
+    @Given("I have login credentials")
+    public void setLoginData(DataTable table) {
+
+        List<Map<String, String>> data = table.asMaps();
+
+        String username = data.get(0).get("username");
+        String password = data.get(0).get("password");
+
+        requestMap = new HashMap<>();
+
+        if (username != null && !username.isEmpty()) {
+            requestMap.put("username", username);
+        }
+
+        if (password != null && !password.isEmpty()) {
+            requestMap.put("password", password);
+        }
+    }
+
+    @Given("I have valid login credentials")
+    public void setValidLogin() {
+
+        requestMap = new HashMap<>();
+        requestMap.put("username", "mor_2314");
+        requestMap.put("password", "83r5^_");
+    }
+
+    @Given("I have username but no password")
+    public void setUsernameOnly() {
+
+        requestMap = new HashMap<>();
+        requestMap.put("username", "john_doe");
+    }
+
+    @Given("I have username {string} and password {string}")
+    public void setCredentials(String username, String password) {
+
+        requestMap = new HashMap<>();
+
+        if (!username.isEmpty()) {
+            requestMap.put("username", username);
+        }
+
+        if (!password.isEmpty()) {
+            requestMap.put("password", password);
+        }
+    }
+
+
+    @When("I send a POST request for login")
+    public void sendLoginRequest() {
+
+        response = RestAssured
+                .given()
+                .header("Content-Type", "application/json")
+                .body(requestMap)
+                .when()
+                .post(Endpoints.AUTH_POST);
+
+        System.out.println("Response: " + response.asString());
+    }
+
     @Then("the response status code should be {int}")
     public void validateStatusCode(int statusCode) {
         Assert.assertEquals(response.getStatusCode(), statusCode);
     }
 
-   
+
     @Then("validate token is generated")
     public void validateToken() {
 
@@ -82,15 +122,19 @@ public class StepDefinitionAuth {
 
         System.out.println("Generated Token: " + token);
 
-        Assert.assertNotNull(token);
-        Assert.assertFalse(token.isEmpty());
+        Assert.assertNotNull(token, "Token is null");
+        Assert.assertFalse(token.isEmpty(), "Token is empty");
     }
 
-  
 
     @Then("the response time should be less than {int} ms in login")
     public void validateResponseTime(int time) {
 
-        Assert.assertTrue(response.getTime() < time);
+        long responseTime = response.getTime();
+
+        System.out.println("Response Time: " + responseTime);
+
+        Assert.assertTrue(responseTime < time,
+                "Response time is greater than expected: " + responseTime);
     }
 }
